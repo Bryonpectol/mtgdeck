dbf540af5eb5a9e9eaafc2e5d47021744b0237a1
Bryon <bryon.pectol@gmail.com>
2021-04-19 19:09:57 -0600

    added player health

--- js/player-health.js
+++ js/player-health.js
@@ -1,2 +1,22 @@
-let playerHealth = document.getElementById("health").value;
+let playerHealth = document.getElementById("health");
+
+let num = 20;
+
+function Add() {
+
+    num += 1;
+
+    playerHealth.textContent = num;
+
+    console.log(playerHealth)
+}
+
+function Subtract() {
+
+    num -= 1;
+
+    playerHealth.textContent = num;
+
+    console.log(playerHealth)
+}
 
